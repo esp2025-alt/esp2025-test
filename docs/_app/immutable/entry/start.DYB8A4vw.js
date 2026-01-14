f6bf089f48a53cd60f418821def5522188a9eb4e
Pavlo V. <pavlo@ArchieBoy.localdomain>
2026-01-14 22:11:53 +0000

    Another try

--- docs/_app/immutable/entry/start.DYB8A4vw.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CUu9lYPm.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 			strict: true
 		}),
 		paths:{
-			base: `/${repoName}`,
+			base: '',
 		},
 		prerender: {
 			handleHttpError: 'warn'
