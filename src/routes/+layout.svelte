4ea4ada1aa272cac122ef2a669e59d85bea692ba
Pavlo V. <pavlo@ArchieBoy.localdomain>
2026-09-11 22:11:17 +0100

    trying other metadata M2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import './layout.css';
 	import "../app.css";
 	import favicon from '$lib/assets/window-blind.svg';
@@ -23,6 +23,26 @@
 		Batalha, Pombal, Ourém e regiões próximas, combinamos conhecimento técnico com um serviço ao cliente ágil para manter a sua propriedade segura, confortável e visualmente apelativa. 
 		Com anos de experiência na indústria, preços competitivos e um compromisso com a excelência, somos os profissionais de estandartes de referência no distrito de Leiria para instalações, manutenção e reparações urgentes.
 		Escolha uma empresa local que priorize o artesanato de qualidade e resultados duradouros para cada tipo de sistema de persianas." />
+</svelte:head> -->
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="canonical" href="https://estoresemproblema.com/" />
+
+	<title>Reparação de Estores em Leiria | Estores Sem Problema</title>
+
+	<meta
+		name="description"
+		content="Reparação e montagem de estores em Leiria. Reparação de estores manuais e elétricos, interiores e exteriores, com serviço rápido e profissional."
+	/>
+
+	<meta property="og:title" content="Reparação de Estores em Leiria | Estores Sem Problema" />
+	<meta
+		property="og:description"
+		content="Reparação e montagem de estores em Leiria. Reparação de estores manuais e elétricos, interiores e exteriores, com serviço rápido e profissional."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://estoresemproblema.com/" />
 </svelte:head>
 
 {@render children()}
