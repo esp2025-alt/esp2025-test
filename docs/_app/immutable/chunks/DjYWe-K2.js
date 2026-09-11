2d03c26f6137f67d4d0a2bd7c50681fb3d1fe4cf
Pavlo V. <pavlo@ArchieBoy.localdomain>
2026-09-11 23:01:21 +0100

    forgot the docs

---
+++ docs/_app/immutable/chunks/DjYWe-K2.js
@@ -0,0 +1,1 @@
+import{b8 as a,k as o,ah as t,ay as c,af as l}from"./DutK3RIE.js";function s(n){t===null&&a(),c&&t.l!==null?u(t).m.push(n):o(()=>{const e=l(n);if(typeof e=="function")return e})}function u(n){var e=n.l;return e.u??={a:[],b:[],m:[]}}export{s as o};
--- docs/_app/immutable/entry/start.DVVMP5RR.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BLnAa2JT.js";export{o as load_css,r as start};
